bfba83901a088d9bddf6e29d0cdd392f510d13b0
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2019-12-08 19:50:24 +0100

    fix main so it compiles

--- Main.hs
+++ Main.hs
@@ -6,24 +6,9 @@ main = do
   initGUI
   icon <- statusIconNewFromStock stockQuit
   statusIconSetVisible icon True
-  statusIconSetTooltip icon "This is a test"
   menu <- mkmenu icon
-  I.onPopupMenu icon $ \b a -> do
-         widgetShowAll menu
-         print (b,a)
-         menuPopup menu $ maybe Nothing (\b' -> Just (b',a)) b
-  I.onActivate icon $ do
-         putStrLn "'activate' signal triggered"
   mainGUI
 
 mkmenu s = do
   m <- menuNew
-  mapM_ (mkitem m) [("Let's blink!",statusIconSetBlinking s True)
-                   ,("Let's stop blink!",statusIconSetBlinking s False)
-                   ,("Quit",mainQuit)]
   return m
-    where
-        mkitem menu (label,act) =
-            do i <- menuItemNewWithLabel label
-               menuShellAppend menu i
-               i `onActivateLeaf` act
